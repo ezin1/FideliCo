ecb334fe3c46d1a8f77f1ea9bbcfeb5fb92ee9f7
ezin1 <eziofeitt15@gmail.com>
2023-05-09 21:57:55 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const hamburguerBtn = document.querySelector('#hamburguer-btn');
+const modal = document.querySelector('#modal');
+const closeBtn = document.querySelector('.close');
+
+hamburguerBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
